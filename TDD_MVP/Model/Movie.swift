c730a172e4d036733570fa1a33cd7fa35636d373
Jamil Shamar <jameelshammr@gmail.com>
2019-12-28 11:57:21 +0800

    implement HTTPStubs- UITest

---
+++ TDD_MVP/Model/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  TDD_MVP
+//
+//  Created by Gamil Ali Qaid Shamar on 28/12/2019.
+//  Copyright © 2019 Jamil. All rights reserved.
+//
+
+import UIKit
+
+class Movie: NSObject {
+
+}
