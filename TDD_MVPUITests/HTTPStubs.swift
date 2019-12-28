d2ce8e7e8e660c88df94f5cfa4b7685719ad4d1a
Jamil Shamar <jameelshammr@gmail.com>
2019-12-28 11:35:36 +0800

    add HTTPStubs

---
+++ TDD_MVPUITests/HTTPStubs.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPStubs.swift
+//  TDD_MVPUITests
+//
+//  Created by Gamil Ali Qaid Shamar on 28/12/2019.
+//  Copyright © 2019 Jamil. All rights reserved.
+//
+
+import UIKit
+
+class HTTPStubs: NSObject {
+
+}
