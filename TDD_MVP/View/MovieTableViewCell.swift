7b1ecdc96077100c36ced0ad02441c2129c167c2
Jamil Shamar <jameelshammr@gmail.com>
2019-12-28 13:16:12 +0800

    corrent rating property

---
+++ TDD_MVP/View/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  TDD_MVP
+//
+//  Created by Gamil Ali Qaid Shamar on 28/12/2019.
+//  Copyright © 2019 Jamil. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
