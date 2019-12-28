860d861cdab7098f47f4b0ab85caf7071b7dca50
Jamil Shamar <jameelshammr@gmail.com>
2019-12-28 12:04:33 +0800

    fix missing files issue

--- TDD_MVPUITests/TDD_MVPUITests.swift
+++ TDD_MVPUITests/TDD_MVPUITests.swift
@@ -9,19 +9,21 @@
 import XCTest
 
 class TDD_MVPUITests: XCTestCase {
+    let app = XCUIApplication()
+    let dynamicStubs = HTTPDynamicStubs()
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+     override func setUp() {
+           continueAfterFailure = false
+           dynamicStubs.setUp()
+           app.launchEnvironment = ["BASEURL" : "http://localhost:8080", "isUITest":"true"]
+           continueAfterFailure = false
+           super.setUp()
+       }
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+       override func tearDown() {
+           super.tearDown()
+           dynamicStubs.tearDown()
+       }
 
 //    func testExample() {
 //        // UI tests must launch the application that they test.
@@ -41,8 +43,6 @@ class TDD_MVPUITests: XCTestCase {
 //        }
 //    }
     
-    
-    
-    
+   
     
 }
